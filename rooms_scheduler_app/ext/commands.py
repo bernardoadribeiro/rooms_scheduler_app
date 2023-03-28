972101628e3cef845d69f1a60310c533e6de8d81
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-03-28 00:11:46 -0300

    chore: remove populate_db

--- rooms_scheduler_app/ext/commands.py
+++ rooms_scheduler_app/ext/commands.py
@@ -1,7 +1,6 @@
 import click
 from rooms_scheduler_app.ext.database import db
 from rooms_scheduler_app.ext.auth import create_user
-from rooms_scheduler_app.ext.database import TypeProduct, Product
 
 
 def create_db():
@@ -16,40 +15,7 @@ def drop_db():
 
 def populate_db():
     """Populate db with sample data"""
-    tp = [
-            TypeProduct(
-                id=1,
-                description="Tipo produto 1"),
-            TypeProduct(
-                id=2,
-                description="Tipo produto 2")
-        ]
-
-    data = [
-        tp[0],
-        tp[1],
-        Product(
-            id=1, 
-            name="Produto 1", 
-            price="10.00", 
-            description="Produto 1 para teste",
-            type_id=tp[0].id),
-        Product(
-            id=2, 
-            name="Produto 2", 
-            price="20.00", 
-            description="Produto 2 para teste",
-            type_id=tp[0].id),
-        Product(
-            id=3, 
-            name="Produto 3", 
-            price="30.00", 
-            description="Produto 3 para teste",
-            type_id=tp[1].id),
-    ]
-    db.session.bulk_save_objects(data)
-    db.session.commit()
-    return Product.query.all()
+    pass
 
 
 def init_app(app):
