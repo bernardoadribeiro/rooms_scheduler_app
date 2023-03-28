84f366bec9c8fd90c9860f43d18c2548c34483eb
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-03-28 00:28:48 -0300

    feat: load models on db database config and admin

--- rooms_scheduler_app/ext/admin.py
+++ rooms_scheduler_app/ext/admin.py
@@ -4,7 +4,7 @@ from flask_admin.contrib import sqla
 from flask_simplelogin import login_required
 from werkzeug.security import generate_password_hash
 
-from rooms_scheduler_app.ext.database import db, Product, User
+from rooms_scheduler_app.ext.database import db, Room, RoomType, User, UserRoomPermission, Schedule
 
 # Proteger o admin com login via Monkey Patch
 AdminIndexView._handle_view = login_required(AdminIndexView._handle_view)
@@ -24,5 +24,8 @@ def init_app(app):
     admin.name = app.config.TITLE
     admin.template_mode = "bootstrap3"
     admin.init_app(app)
-    admin.add_view(sqla.ModelView(Product, db.session))
+    admin.add_view(sqla.ModelView(Room, db.session))
+    admin.add_view(sqla.ModelView(RoomType, db.session))
+    admin.add_view(sqla.ModelView(Schedule, db.session))
+    admin.add_view(sqla.ModelView(UserRoomPermission, db.session))
     admin.add_view(UserAdmin(User, db.session))
--- rooms_scheduler_app/ext/database.py
+++ rooms_scheduler_app/ext/database.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
@@ -9,25 +8,6 @@ def init_app(app):
     migrate = Migrate(app, db, directory='./rooms_scheduler_app/migrations')
     db.init_app(app)
 
-class TypeProduct(db.Model, SerializerMixin):
-    __tablename__ = "type_product"
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(150))
-
-class Product(db.Model, SerializerMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(140))
-    price = db.Column(db.Numeric())
-    description = db.Column(db.Text)
-    type_id = db.Column(db.Integer, db.ForeignKey('type_product.id'),
-        nullable=False)
-    type = db.relationship('TypeProduct', lazy=True)
-
-
-class User(db.Model, SerializerMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String())
-    last_name = db.Column(db.String())
-    email = db.Column(db.String())
-    username = db.Column(db.String(140))
-    password = db.Column(db.String(512))
+from ..models.user import User, UserRoomPermission
+from ..models.room import Room, RoomType
+from ..models.schedule import Schedule
