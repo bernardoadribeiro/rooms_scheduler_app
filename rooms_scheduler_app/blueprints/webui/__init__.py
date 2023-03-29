f0723ab4472126576dca75d7022f7ffd0d720cfe
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-03-29 16:47:42 -0300

    feat: schedules webui

--- rooms_scheduler_app/blueprints/webui/__init__.py
+++ rooms_scheduler_app/blueprints/webui/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from .views import index, product
+from .views import index, schedule
 
 bp = Blueprint("webui", __name__, template_folder="templates",
     url_prefix="/main/v1"
@@ -7,7 +7,7 @@ bp = Blueprint("webui", __name__, template_folder="templates",
 
 bp.add_url_rule('/', view_func=index)
 bp.add_url_rule(
-    "/product/<product_id>", view_func=product, endpoint="productview"
+    "/schedules/<schedule_id>", view_func=schedule, endpoint="scheduleview"
 )
 
 def init_app(app):
