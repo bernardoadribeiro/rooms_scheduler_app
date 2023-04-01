c6974fbd1fd31b73ea4d9508b2ca58648bf8ac7e
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-04-01 12:52:39 -0300

    fix: index route

--- rooms_scheduler_app/blueprints/webui/__init__.py
+++ rooms_scheduler_app/blueprints/webui/__init__.py
@@ -2,13 +2,15 @@ from flask import Blueprint
 from .views import index, schedule
 
 bp = Blueprint("webui", __name__, template_folder="templates",
-    url_prefix="/main/v1"
-)
+               url_prefix="/"
+               )
 
 bp.add_url_rule('/', view_func=index)
+bp.add_url_rule('/main/v1', view_func=index)
 bp.add_url_rule(
-    "/schedules/<schedule_id>", view_func=schedule, endpoint="scheduleview"
+    "/main/v1/schedules/<schedule_id>", view_func=schedule, endpoint="scheduleview"
 )
+
 
 def init_app(app):
     app.register_blueprint(bp)
